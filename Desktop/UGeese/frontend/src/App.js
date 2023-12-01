22e7df88269b5d3037772f832e327134cce31561
Gail Parayno <gailparayno@Gails-MacBook-Air.local>
2023-12-01 14:35:46 +0900

    Fixed signup

--- Desktop/UGeese/frontend/src/App.js
+++ Desktop/UGeese/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, useContext } from 'react';
+
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import SigninForm from './auth/forms/signin';
 import SignUpForm from './auth/forms/signup';
